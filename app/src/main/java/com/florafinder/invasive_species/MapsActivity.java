4d54bbe90089168704d96f105c8a84e13cf11e9a
Ryan Novotny <novot039@d.umn.edu>
2016-10-20 11:05:18 -0500

    Changed default location to Duluth, MN (46.7867, -92.1005)
    Changed newLatLng(sydney) to newLatLngZoom(duluth, 15.0f) to account for the default location and zoom height of the map.

--- app/src/main/java/com/florafinder/invasive_species/MapsActivity.java
+++ app/src/main/java/com/florafinder/invasive_species/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng duluth = new LatLng(46.7867, -92.1005);
+        mMap.addMarker(new MarkerOptions().position(duluth).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(duluth, 15.0f));
     }
 }
