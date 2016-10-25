c37ba6b05293af085a7f0f45089058113ef03d4d
Ryan Novotny <novot039@d.umn.edu>
2016-10-24 19:44:00 -0500

    Set ActionBar title to "Invasive Species"

--- app/src/main/java/com/florafinder/invasive_species/MapsActivity.java
+++ app/src/main/java/com/florafinder/invasive_species/MapsActivity.java
@@ -32,6 +32,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.action_bar, menu);
+        setTitle("Invasive Species");
         return true;
     }
 
