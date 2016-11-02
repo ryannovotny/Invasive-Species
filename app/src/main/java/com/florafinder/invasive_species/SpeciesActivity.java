ff2d6b70190ef6c1c49742924f130c07305b8c96
Ryan Novotny <novot039@d.umn.edu>
2016-11-02 16:41:20 -0500

    Added class definitions for both Species List and Settings list, along with their respective layouts as well.
    Called from Navigation Drawer, sets new intent and initializes

---
+++ app/src/main/java/com/florafinder/invasive_species/SpeciesActivity.java
@@ -0,0 +1,18 @@
+package com.florafinder.invasive_species;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ryan on 11/2/16.
+ */
+
+public class SpeciesActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_species);
+    }
+}
