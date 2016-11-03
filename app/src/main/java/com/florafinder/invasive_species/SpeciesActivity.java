1c81269bff8d6143c199bce539fb1906ea118d49
Landon Nelson <nels8526@d.umn.edu>
2016-11-03 00:02:20 -0500

    Added a card to the species list. Will add more next time around

--- app/src/main/java/com/florafinder/invasive_species/SpeciesActivity.java
+++ app/src/main/java/com/florafinder/invasive_species/SpeciesActivity.java
@@ -14,5 +14,6 @@ public class SpeciesActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_species);
+
     }
 }
