f50cb49d8e75e8f535533b6c5cb0d4957b35f3db
Edward FitzSimons <fitzs045@d.umn.edu>
2016-12-05 15:06:42 -0600

    Stores and checks for existence of tiles by lat lang

---
+++ app/src/main/java/com/florafinder/invasive_species/InvPolygonList.java
@@ -0,0 +1,45 @@
+package com.florafinder.invasive_species;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by Fitzy on 12/5/2016.
+ */
+
+public class InvPolygonList{
+
+    private final ArrayList<InvPolygon> list = new ArrayList<InvPolygon>();
+
+    public InvPolygonList() {
+    }
+
+    /**
+     * Adds polygon by lat and lang
+     * @param lat
+     * @param lang
+     */
+    public void add(double lat, double lang) {
+        list.add(new InvPolygon(lat, lang));
+    }
+
+    /**
+     * Adds polygon
+     * @param polygon
+     */
+    public void add(InvPolygon polygon){
+        list.add(polygon);
+    }
+
+    public boolean hasTile(double lat, double lang) {
+        boolean exists = false;
+
+        for(int i = 0; i < list.size() && !exists; ++i) {
+            if(list.get(i).hasLatLang(lat,lang)){
+                exists = true;
+            }
+        }
+
+        return exists;
+    }
+}
