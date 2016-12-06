fef8421eaac0204eee82c81fe8a2c61b9359adc9
Ryan Novotny <novot039@d.umn.edu>
2016-12-05 22:35:33 -0600

    Added a toolbar whose name changes per fragment

--- app/src/main/java/com/florafinder/invasive_species/MainActivity.java
+++ app/src/main/java/com/florafinder/invasive_species/MainActivity.java
@@ -3,25 +3,34 @@ package com.florafinder.invasive_species;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatCallback;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-/**
- * Created by ryano on 12/5/2016.
- */
-
 public class MainActivity extends FragmentActivity implements
-        MapFragment.OnFragmentInteractionListener, SpeciesFragment.OnFragmentInteractionListener {
+        MapFragment.OnFragmentInteractionListener, SpeciesFragment.OnFragmentInteractionListener,
+        AppCompatCallback {
 
     private Fragment fragment;
+    private AppCompatDelegate delegate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        delegate = AppCompatDelegate.create(this, this);
+        delegate.onCreate(savedInstanceState);
+        delegate.setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        delegate.setSupportActionBar(toolbar);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -31,9 +40,11 @@ public class MainActivity extends FragmentActivity implements
                 switch (id){
                     case R.id.bottom_nav_map:
                         fragment = new MapFragment();
+                        toolbar.setTitle(R.string.map);
                         break;
                     case R.id.bottom_nav_species:
                         fragment = new SpeciesFragment();
+                        toolbar.setTitle(R.string.species);
                         break;
                     case R.id.bottom_nav_settings:
 
@@ -47,6 +58,7 @@ public class MainActivity extends FragmentActivity implements
         });
 
         fragment = new MapFragment();
+        toolbar.setTitle(R.string.map);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.main_container, fragment).commit();
     }
@@ -54,5 +66,21 @@ public class MainActivity extends FragmentActivity implements
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public void onSupportActionModeStarted(ActionMode mode) {
+
+    }
+
+    @Override
+    public void onSupportActionModeFinished(ActionMode mode) {
+
+    }
+
+    @Nullable
+    @Override
+    public ActionMode onWindowStartingSupportActionMode(ActionMode.Callback callback) {
+        return null;
     }
 }
