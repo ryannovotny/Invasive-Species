653bc48758e338bb6b69d21fea978bd2fba2cb8f
Edward FitzSimons <fitzs045@d.umn.edu>
2016-12-05 21:50:47 -0600

    List creates. Is killed when item is selected

--- app/src/main/java/com/florafinder/invasive_species/SpeciesPickerActivity.java
+++ app/src/main/java/com/florafinder/invasive_species/SpeciesPickerActivity.java
@@ -1,13 +1,24 @@
 package com.florafinder.invasive_species;
 
+import android.app.Activity;
+import android.app.LauncherActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SpeciesPickerActivity extends AppCompatActivity {
+
+    private double lat, lang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +35,38 @@ public class SpeciesPickerActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        lat = getIntent().getExtras().getDouble("lat");
+        lang = getIntent().getExtras().getDouble("lang");
+    }
+
+    private void setUpList(){
+
+        final ListView view = (ListView) findViewById(R.id.list_species);
+        String[] values = new String[] {"BuckThorn"};
+
+        final ArrayList<String> listStrings = new ArrayList();
+
+        for(String string: values){
+            listStrings.add(string);
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, listStrings);
+        view.setAdapter(adapter);
+    }
+
+    /**
+     * Return to the map with a selected string
+     * @param view
+     */
+    public void returnToMap(View view){
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("species", ((TextView) view).getText());
+        returnIntent.putExtra("lat", lat);
+        returnIntent.putExtra("lang", lang);
+        setResult(Activity.RESULT_OK,returnIntent);
+
+        this.finish(); //kill activity
     }
 }
