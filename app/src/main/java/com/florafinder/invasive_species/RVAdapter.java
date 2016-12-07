87de01485d177ea951457ca06f5d550f3e0973ac
Ryan Novotny <novot039@d.umn.edu>
2016-12-07 14:28:18 -0600

    Cleaned up code

--- app/src/main/java/com/florafinder/invasive_species/RVAdapter.java
+++ app/src/main/java/com/florafinder/invasive_species/RVAdapter.java
@@ -1,6 +1,10 @@
 package com.florafinder.invasive_species;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.constraint.solver.SolverVariable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,9 +15,9 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
+class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
 
-    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+    static class PersonViewHolder extends RecyclerView.ViewHolder {
 
         CardView cv;
         TextView speciesName;
@@ -26,7 +30,6 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
         PersonViewHolder(View itemView) {
             super(itemView);
             cv = (CardView)itemView.findViewById(R.id.cv);
-            //cv.setCardBackgroundColor(Color.rgb(142,210,122));
 
             speciesName = (TextView)itemView.findViewById(R.id.species_name);
             speciesScientific = (TextView) itemView.findViewById(R.id.species_scientific);
@@ -34,8 +37,6 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
             speciesPhoto = (ImageView)itemView.findViewById(R.id.species_photo);
             //speciesRemove= (TextView)itemView.findViewById(R.id.species_remove);
             //Link= (TextView)itemView.findViewById(R.id.species_link);
-
-            //speciesScientific.setTextColor(Color.alpha(54));
         }
     }
 
@@ -59,14 +60,12 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> 
 
     @Override
     public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
-        personViewHolder.speciesName.setText(species.get(i).name);
+        personViewHolder.speciesName.setText(species.get(i).name);;
         personViewHolder.speciesScientific.setText(species.get(i).scienceName);
         personViewHolder.speciesDescription.setText(species.get(i).description);
         personViewHolder.speciesPhoto.setImageResource(species.get(i).photoId);
         //personViewHolder.speciesRemove.setText(species.get(i).remove);
         //personViewHolder.Link.setText(species.get(i).link);
-
-
     }
 
     @Override
