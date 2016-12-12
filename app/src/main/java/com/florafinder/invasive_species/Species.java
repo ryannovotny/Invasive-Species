4fb61e2150896c74b83f763554f3e345bd0b5a4f
Ryan Novotny <novot039@d.umn.edu>
2016-12-11 23:14:17 -0600

    Cleaned up class momentarily as other variables not needed

--- app/src/main/java/com/florafinder/invasive_species/Species.java
+++ app/src/main/java/com/florafinder/invasive_species/Species.java
@@ -5,15 +5,11 @@ class Species {
     int scienceName;
     int description;
     int photoId;
-    int remove;
-    int link;
 
-    Species(int name, int scienceName, int description, int photoId, int remove, int link) {
+    Species(int name, int scienceName, int description, int photoId) {
         this.name = name;
         this.scienceName = scienceName;
         this.description = description;
         this.photoId = photoId;
-        this.remove = remove;
-        this.link = link;
     }
 }
